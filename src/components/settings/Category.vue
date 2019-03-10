85b346620a63b179368d3354ecf387b31383b700
郑保乐 <1146556298@qq.com>
2019-03-11 00:01:00 +0800

    签到、我的统计、我的邮箱

--- src/components/settings/Category.vue
+++ src/components/settings/Category.vue
@@ -2,11 +2,7 @@
   <v-ons-page>
     <Title back title="分类管理"/>
     <div class="wrapper">
-      <ul class="list-group" v-sortable="{ handle: '.handle' }">
-        <li class="list-group-item">Foo <i class="handle"></i></li>
-        <li class="list-group-item">Bar <i class="handle"></i></li>
-        <li class="list-group-item">Baz <i class="handle"></i></li>
-      </ul>
+      
     </div>
   </v-ons-page>
 </template>
