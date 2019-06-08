5a96e3ebeca36b77d2cd4d5a7c5364ccccafac9b
郑保乐 <1146556298@qq.com>
2019-06-08 20:36:30 +0800

    assembleDebug

--- build-debug.sh
+++ build-debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(cd phonegap/platforms/android && ./gradlew build)
+(cd phonegap/platforms/android && ./gradlew assembleDebug)
 mv phonegap/platforms/android/app/build/outputs/apk/debug/app-debug.apk apk
